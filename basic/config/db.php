4786b66eac26bb2508d70809a8212d386708e524
tsingliuwin <410869548@qq.com>
2015-10-07 11:04:45 +0800

    测试

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=rdsbn7vfmvmjufm.mysql.rds.aliyuncs.com:3306;dbname=eduwind',
+    'username' => 'tsing',
+    'password' => 'tsingliuwin58',
     'charset' => 'utf8',
 ];
